ce49e0f8bcc2709747977be2f5654f353d84ed46
vandanaBvm <vandana.bvminfotech@gmail.com>
2023-04-03 12:56:13 +0530

    Initial Commit

---
+++ usersearch.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>User searchbox</title>
+        @livewireStyles
+    </head>
+    <body>
+        @livewire('searchbox')
+        @livewire('profile')
+        @livewireScripts
+    </body>
+</html>
